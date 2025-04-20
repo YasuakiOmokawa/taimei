24bedaba128d700fee338e56c63b8f5bf03b983b
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-04-20 11:08:07 +0900

    finish tip7

--- lib/sample/e-tip-7.ts
+++ lib/sample/e-tip-7.ts
@@ -1,3 +1,5 @@
+import { number } from "zod";
+
 type Person = {
   name: string;
   age: number;
@@ -10,3 +12,19 @@ interface _PersonSpan extends Person {
 interface _WorkerSpan extends Omit<Person, "job"> {
   job: string;
 }
+
+interface NullyStudent {
+  name: string;
+  ageYears: number | null;
+}
+interface _Student extends NullyStudent {
+  ageYears: number;
+}
+
+type NullyStudent2 = {
+  name: string;
+  ageYears: number | null;
+};
+type _Student2 = Omit<NullyStudent2, "ageYears"> & {
+  ageYears: number;
+};
