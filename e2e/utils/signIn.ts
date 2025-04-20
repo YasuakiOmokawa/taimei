32f3efb4825c7ebf294168dcea5c2909a69c1a41
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-04-20 12:18:01 +0900

    split fake signIn

---
+++ e2e/utils/signIn.ts
@@ -0,0 +1,32 @@
+import { Browser, BrowserContext } from "@playwright/test";
+import { prisma } from "@/prisma";
+
+export async function signIn(browser: Browser): Promise<BrowserContext> {
+  await setTestUser();
+  const context = await browser.newContext();
+  context.addCookies([
+    {
+      name: "authjs.session-token",
+      value: "dummy",
+      domain: "localhost",
+      path: "/",
+    },
+  ]);
+
+  return context;
+}
+
+async function setTestUser() {
+  if (
+    (await prisma.user.count({ where: { email: "user@example.com" } })) === 0
+  ) {
+    await prisma.user.create({
+      data: {
+        id: "test123456",
+        name: "Test Example",
+        email: "user@example.com",
+        image: "https://avatars.githubusercontent.com/u/000000",
+      },
+    });
+  }
+}
