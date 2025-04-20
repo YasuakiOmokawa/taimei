0f1ab5fe6609e34d462f2b5d28c4536e7d97001b
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-04-20 12:18:47 +0900

    split to beforeAll with signIn

--- e2e/dashboard.spec.ts
+++ e2e/dashboard.spec.ts
@@ -1,42 +1,20 @@
 import { test, expect } from "@playwright/test";
 import { DashboardPage } from "./pages/dashboard-page";
-import { prisma } from "@/prisma";
+import { signIn } from "./utils/signIn";
 
 test.describe("app/dashboard/page.tsx", () => {
-  // this is reused by all tests in the file.
-  let dashboadPage: DashboardPage;
+  let dashboardPage: DashboardPage;
 
-  test.beforeEach(async ({ browser }) => {
-    // テストユーザーのデータを用意
-    if (
-      (await prisma.user.count({ where: { email: "user@example.com" } })) === 0
-    ) {
-      await prisma.user.create({
-        data: {
-          id: "test123456",
-          name: "Test Example",
-          email: "user@example.com",
-          image: "https://avatars.githubusercontent.com/u/000000",
-        },
-      });
-    }
+  test.beforeAll(async ({ browser }) => {
+    const browserContext = await signIn(browser);
+    dashboardPage = new DashboardPage(await browserContext.newPage());
+  });
 
-    // ブラウザにセッションのCookieを保存する
-    const context = await browser.newContext();
-    await context.addCookies([
-      {
-        name: "authjs.session-token",
-        value: "dummy",
-        domain: "localhost",
-        path: "/",
-      },
-    ]);
-
-    dashboadPage = new DashboardPage(await context.newPage());
-    await dashboadPage.goto();
+  test.beforeEach(async () => {
+    await dashboardPage.goto();
   });
 
   test("has heading title", async () => {
-    await expect(dashboadPage.getHeadingPageTitle()).toBeVisible();
+    await expect(dashboardPage.getHeadingPageTitle()).toBeVisible();
   });
 });
