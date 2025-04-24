f3a8fe762762e70716cbbcc33d592534b499a2fc
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-04-24 09:04:22 +0900

    tries unknown object to something typed objects

--- lib/sample/e-tip-7.ts
+++ lib/sample/e-tip-7.ts
@@ -1,4 +1,4 @@
-import { number } from "zod";
+import { number, string } from "zod";
 
 type Person = {
   name: string;
@@ -21,10 +21,56 @@ interface _Student extends NullyStudent {
   ageYears: number;
 }
 
-type NullyStudent2 = {
-  name: string;
-  ageYears: number | null;
+type MasterIndex = {
+  itemKey: string;
+  masterId: number | null;
 };
-type _Student2 = Omit<NullyStudent2, "ageYears"> & {
-  ageYears: number;
+
+const metas: Record<string, unknown>[] = [
+  { item_key: undefined, master_id: 0 },
+  { item_key: "", master_id: null },
+  { item_key: "a", master_id: null },
+  { item_key: "", master_id: 1 },
+  { item_key: "aa", master_id: "9" },
+  { item_key2: "b", master_id: "a" },
+  { item_key: "", master_id2: 3 },
+];
+
+const masterIndexes: MasterIndex[] = metas.map((meta) => ({
+  itemKey: typeof meta.item_key === "string" ? meta.item_key : "",
+  masterId: typeof meta.master_id === "number" ? meta.master_id : null,
+}));
+console.log("masterIndexes:");
+console.table(masterIndexes);
+
+const uniqueMasterIndexes = masterIndexes.filter(
+  (masterIndex, index) =>
+    index ===
+    masterIndexes.findIndex(
+      (other) =>
+        masterIndex.itemKey === other.itemKey &&
+        masterIndex.masterId === other.masterId
+    )
+);
+console.log("uniqueMasterIndexes:");
+console.table(uniqueMasterIndexes);
+
+const buildUniqueMasterIndexes = (
+  metas: Record<string, unknown>[]
+): MasterIndex[] => {
+  const masterIndexes: MasterIndex[] = metas.map((meta) => ({
+    itemKey: typeof meta.item_key === "string" ? meta.item_key : "",
+    masterId: typeof meta.master_id === "number" ? meta.master_id : null,
+  }));
+  return masterIndexes.filter(
+    (masterIndex, index) =>
+      index ===
+      masterIndexes.findIndex(
+        (other) =>
+          masterIndex.itemKey === other.itemKey &&
+          masterIndex.masterId === other.masterId
+      )
+  );
 };
+
+console.table(buildUniqueMasterIndexes(metas));
