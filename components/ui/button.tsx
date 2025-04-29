3f9f2aa9a29435f5527004d95af7d99c7f7be230
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-04-29 19:43:14 +0900

    configure outside component mount

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -6,6 +6,8 @@ import { cn } from "@/lib/utils";
 import { useFormStatus } from "react-dom";
 import { BProgress } from "@bprogress/core";
 
+BProgress.configure({ showSpinner: false });
+
 const buttonVariants = cva(
   "inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0",
   {
@@ -47,7 +49,6 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const isSending = status.pending && props["type"] === "submit";
     props["children"] = isSending ? "Sending..." : props["children"];
     const Comp = asChild ? Slot : "button";
-    BProgress.configure({ showSpinner: false });
 
     React.useEffect(() => {
       if (isSending) {
