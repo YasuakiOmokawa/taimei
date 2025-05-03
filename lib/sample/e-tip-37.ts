b04952680eb8a30c57af44a42815beef2856d6d7
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-05-03 09:31:36 +0900

    add unitSystem prop

--- lib/sample/e-tip-37.ts
+++ lib/sample/e-tip-37.ts
@@ -1,6 +1,12 @@
+type UnitSystem = "metric" | "imperial";
+interface AppConfig {
+  darkMode: boolean;
+  unitSystem: UnitSystem;
+}
 interface FormattedValue {
   value: number;
   units: string;
+  unitSystem?: UnitSystem;
 }
 function formatValue(value: FormattedValue) {
   return value.value;
@@ -10,10 +16,17 @@ interface Hike {
   miles: number;
   hours: number;
 }
-function formatHike({ miles, hours }: Hike) {
-  const distanceDisplay = formatValue({ value: miles, units: "miles" });
+function formatHike({ miles, hours }: Hike, config: AppConfig) {
+  const { unitSystem } = config;
+  const distanceDisplay = formatValue({
+    value: miles,
+    units: "miles",
+    unitSystem,
+  });
   const paceDisplay = formatValue({ value: miles / hours, units: "mph" });
   return `${distanceDisplay} at ${paceDisplay}`;
 }
 
-console.log(formatHike({ miles: 1, hours: 1 }));
+console.log(
+  formatHike({ miles: 1, hours: 1 }, { darkMode: true, unitSystem: "metric" })
+);
