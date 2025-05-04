954941de70c5317eb7c194405df950bdb6990f69
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-05-04 21:31:12 +0900

    extract functions to custom hook and fallback avatar without avatarPreview

--- app/setting/profile/avatar-upload.tsx
+++ app/setting/profile/avatar-upload.tsx
@@ -1,17 +1,13 @@
 "use client";
 
-import type React from "react";
+import React from "react";
 
-import { useRef } from "react";
 import { Button } from "@/components/ui/button";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Trash2 } from "lucide-react";
 import { FieldMetadata } from "@conform-to/react";
 import { useAvatar } from "./useAvatar";
-import { deleteAvatar } from "@/app/lib/actions";
-import { toast } from "sonner";
-import { BProgress } from "@bprogress/core";
-// import { deleteAvatar } from "@/app/lib/actions";
+import { getInitials } from "./utils";
 
 interface Props {
   avatarUrl: string;
@@ -26,53 +22,24 @@ export function AvatarUpload({
   avatarField,
   avatarUrlField,
 }: Props) {
-  const { avatarPreview, updatePreview, getInitials, setAvatarPreview } =
+  const { avatarPreview, updatePreview, fileInputRef, handlDeleteAvatar } =
     useAvatar(avatarUrl);
-  const fileInputRef = useRef<HTMLInputElement>(null);
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    updatePreview(e);
-  };
-
-  const handleDeleteAvatar = async () => {
-    // アバターがDBにない状態でプレビューを更新した場合の対応
-    if (!avatarUrl) {
-      setAvatarPreview(null);
-      if (fileInputRef.current) fileInputRef.current.value = "";
-      return;
-    }
-
-    BProgress.start();
-    const result = await deleteAvatar(avatarUrl);
-    if (result.status === "success") {
-      setAvatarPreview(null);
-      if (fileInputRef.current) fileInputRef.current.value = "";
-      toast.success("アバターを削除しました");
-    } else {
-      toast.error(result.message ?? "予期せぬエラーが発生しました");
-    }
-    BProgress.done();
-  };
 
   return (
     <div className="flex flex-col items-center gap-4 p-4 rounded-lg bg-muted/10">
       <div className="relative">
         <Avatar className="w-28 h-28 border-2 border-muted">
-          {avatarPreview ? (
-            <AvatarImage
-              src={avatarPreview ?? "/placeholder.svg"}
-              alt="プロフィール画像"
-            />
-          ) : (
-            <AvatarFallback>{getInitials(userName)}</AvatarFallback>
-          )}
+          <AvatarImage src={avatarPreview} alt="プロフィール画像" />
+          <AvatarFallback>{getInitials(userName)}</AvatarFallback>
         </Avatar>
         {avatarPreview && (
           <Button
             variant="destructive"
             size="icon"
             className="absolute -top-2 -right-2 h-8 w-8 rounded-full"
-            onClick={handleDeleteAvatar}
+            onClick={() => {
+              handlDeleteAvatar();
+            }}
             type="button"
           >
             <Trash2 className="h-4 w-4" />
@@ -90,7 +57,9 @@ export function AvatarUpload({
           key={avatarField.key}
           accept="image/png, image/jpeg, image/jpg, image/webp"
           className="hidden"
-          onChange={handleFileChange}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            updatePreview(e);
+          }}
           ref={fileInputRef}
         />
         <input type="hidden" name={avatarUrlField.name} value={avatarUrl} />
