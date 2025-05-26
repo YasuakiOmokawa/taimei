aa0b74f077ad4fd863120bef9928bf51500a16b1
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-05-26 14:59:33 +0900

    treat fail with Effect.fail

--- lib/sample/try-effect.ts
+++ lib/sample/try-effect.ts
@@ -7,14 +7,13 @@ type _E = Effect.Effect.Error<typeof _program>;
 type _R = Effect.Effect.Context<typeof _program>;
 
 // Type signature doesn't show possible exceptions
-const divide = (a: number, b: number): number => {
-  if (b === 0) {
-    throw new Error("Cannot divide by zero");
-  }
-  return a / b;
-};
+const divide = (a: number, b: number): Effect.Effect<number, Error> =>
+  b === 0
+    ? Effect.fail(new Error("Cannot divide by zero"))
+    : Effect.succeed(a / b);
 
 console.log(divide(1, 0));
+console.log(divide(1, 1));
 const _success = Effect.succeed(42);
 const _failure = Effect.fail(new Error("failed due to some error."));
 
