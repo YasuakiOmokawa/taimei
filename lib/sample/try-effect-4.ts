f4e4c85a372d4befde0ff16ac3ae70d46a7bf84c
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-06-04 18:50:52 +0900

    use effect flatmap

--- lib/sample/try-effect-4.ts
+++ lib/sample/try-effect-4.ts
@@ -18,3 +18,21 @@ Effect.runPromise(finalAmount).then(console.log);
 // as
 const asProgram = pipe(Effect.succeed(5), Effect.as("new value"));
 Effect.runPromise(asProgram).then(console.log);
+
+// flatMap
+const applyDiscount = (
+  total: number,
+  discountRate: number
+): Effect.Effect<number, Error> =>
+  discountRate === 0
+    ? Effect.fail(new Error("invalid of zero"))
+    : Effect.succeed(total - (total * discountRate) / 100);
+
+const fetchTransactionAmount2 = Effect.promise(() => Promise.resolve(100));
+
+const finalAmount2 = pipe(
+  fetchTransactionAmount2,
+  Effect.flatMap((amount) => applyDiscount(amount, 5))
+);
+
+Effect.runPromise(finalAmount2).then(console.log).catch(console.error);
