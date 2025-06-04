761f65b3be1950b0e82d799bf7c1c88cfd4c2ac3
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-06-04 12:57:29 +0900

    impl runExit

--- lib/sample/try-effect-2.ts
+++ lib/sample/try-effect-2.ts
@@ -7,3 +7,9 @@ const program = Effect.sync(() => {
 
 const result = Effect.runSync(program);
 console.log(result);
+
+Effect.runPromise(Effect.succeed(1)).then(console.log);
+Effect.runPromise(Effect.fail("error")).catch(console.error);
+
+Effect.runPromiseExit(Effect.succeed(1)).then(console.log);
+Effect.runPromiseExit(Effect.fail("error")).then(console.log);
