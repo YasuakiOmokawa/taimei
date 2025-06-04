742e59d06175fc5df4494eae079289aecb5074c6
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-06-04 18:09:01 +0900

    impl pipe

---
+++ lib/sample/try-effect-4.ts
@@ -0,0 +1,8 @@
+import { pipe } from "effect";
+
+const increment = (x: number) => x + 1;
+const double = (x: number) => x * 2;
+const subtractTen = (x: number) => x - 10;
+
+const result = pipe(5, increment, double, subtractTen);
+console.log(result);
