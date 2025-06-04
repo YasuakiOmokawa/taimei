dda46e267d020f6d3bc25607a49d6632bfe3798d
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-06-04 13:04:33 +0900

    impl fiber

--- lib/sample/try-effect-2.ts
+++ lib/sample/try-effect-2.ts
@@ -1,4 +1,4 @@
-import { Effect } from "effect";
+import { Effect, Console, Schedule, Fiber } from "effect";
 
 const program = Effect.sync(() => {
   console.log("hello,world");
@@ -13,3 +13,14 @@ Effect.runPromise(Effect.fail("error")).catch(console.error);
 
 Effect.runPromiseExit(Effect.succeed(1)).then(console.log);
 Effect.runPromiseExit(Effect.fail("error")).then(console.log);
+
+const fiberProgram = Effect.repeat(
+  Console.log("running..."),
+  Schedule.spaced("200 millis")
+);
+
+const fiber = Effect.runFork(fiberProgram);
+
+setTimeout(() => {
+  Effect.runFork(Fiber.interrupt(fiber));
+}, 500);
