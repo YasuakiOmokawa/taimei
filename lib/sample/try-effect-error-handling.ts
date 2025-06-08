3400641fc77919a723ee4f51c973c940e87928e9
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-06-08 16:21:59 +0900

    impl union typed error

--- lib/sample/try-effect-error-handling.ts
+++ lib/sample/try-effect-error-handling.ts
@@ -1,12 +1,17 @@
 import { Effect, Random, Data } from "effect";
 
 class HttpError extends Data.TaggedError("HttpError")<{}> {}
+class ValidationError extends Data.TaggedError("ValidationError")<{}> {}
 
 const program = Effect.gen(function* () {
-  const n = yield* Random.next;
+  const n1 = yield* Random.next;
+  const n2 = yield* Random.next;
 
-  if (n < 0.5) {
+  if (n1 < 0.5) {
     return yield* Effect.fail(new HttpError());
+  }
+  if (n2 < 0.5) {
+    return yield* Effect.fail(new ValidationError());
   }
 
   return "some result";
