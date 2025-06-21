2cd4d59c850f80ffbd4e9d5aad4924318ef5399a
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-06-21 15:05:18 +0900

    impl die message

--- lib/sample/try-effect-unexpected-error.ts
+++ lib/sample/try-effect-unexpected-error.ts
@@ -1,9 +1,7 @@
 import { Effect } from "effect";
 
 const divide = (a: number, b: number) =>
-  b === 0
-    ? Effect.die(new Error("cannot divide by zero"))
-    : Effect.succeed(a / b);
+  b === 0 ? Effect.dieMessage("cannot divide by zero") : Effect.succeed(a / b);
 
 const program = divide(1, 0);
 Effect.runPromise(program).catch(console.error);
