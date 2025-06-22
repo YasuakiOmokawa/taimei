6ed8f861454efa5d9c638baf5a6884b8a8635527
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-06-22 12:55:04 +0900

    use first success of

--- lib/sample/try-effect-fallback.ts
+++ lib/sample/try-effect-fallback.ts
@@ -1,4 +1,4 @@
-import { Effect } from "effect";
+import { Effect, Console } from "effect";
 
 const success = Effect.succeed("success");
 const failure = Effect.fail("failure");
@@ -27,3 +27,33 @@ Effect.runPromiseExit(orElseFailProgram).then(console.log);
 // use or else succeed
 const orElseSuccessProgram = Effect.orElseSucceed(validate(-1), () => 18);
 Effect.runPromiseExit(orElseSuccessProgram).then(console.log);
+
+// use first success of
+interface Config {
+  host: string;
+  port: number;
+  apiKey: string;
+}
+
+const makeConfig = (name: string): Config => ({
+  host: `${name}.example.com`,
+  port: 8080,
+  apiKey: "123-456-789",
+});
+
+const remoteConfig = (name: string): Effect.Effect<Config, Error> =>
+  Effect.gen(function* () {
+    if (name === "node3") {
+      yield* Console.log(`Config for ${name} found`);
+      return makeConfig(name);
+    } else {
+      yield* Console.log(`unavailable config for ${name}`);
+      return yield* Effect.fail(new Error(`Config not found ${name}`));
+    }
+  });
+
+const masterConfig = remoteConfig("master");
+const nodeConfigs = ["node1", "node2", "node3", "node4"].map(remoteConfig);
+const config = Effect.firstSuccessOf([masterConfig, ...nodeConfigs]);
+const result = Effect.runSync(config);
+console.log(result);
