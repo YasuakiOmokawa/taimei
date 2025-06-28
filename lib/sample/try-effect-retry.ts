4357a504ff55b0bee04bfd79b25dce0002da2702
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-06-28 14:09:37 +0900

    impl counter closure

--- lib/sample/try-effect-retry.ts
+++ lib/sample/try-effect-retry.ts
@@ -1,19 +1,41 @@
-import { Effect, Schedule } from "effect";
+import { Effect, Schedule, Console } from "effect";
 
-let count = 0;
+const createCounter = () => {
+  let val = 0;
+  return {
+    increment() {
+      val++;
+    },
+    getVal() {
+      return val;
+    },
+  };
+};
 
-const task = Effect.async<string, Error>((resume) => {
-  if (count <= 2) {
-    count++;
-    console.log("failure");
-    resume(Effect.fail(new Error()));
-  } else {
-    console.log("success");
-    resume(Effect.succeed("yay"));
-  }
-});
+const task = (counter: typeof createCounter.prototype) =>
+  Effect.async<string, Error>((resume) => {
+    if (counter.getVal() <= 2) {
+      counter.increment();
+      console.log("failure");
+      resume(Effect.fail(new Error()));
+    } else {
+      console.log("success");
+      resume(Effect.succeed("yay"));
+    }
+  });
 
 // simple retry
 const policy = Schedule.fixed("100 millis");
-const repeated = Effect.retry(task, policy);
+const repeated = Effect.retry(task(createCounter()), policy);
 Effect.runPromise(repeated).then(console.log);
+
+const addDelayPolicy = Schedule.addDelay(
+  Schedule.recurs(2),
+  () => "100 millis"
+);
+const delayRepeated = Effect.retryOrElse(
+  task(createCounter()),
+  addDelayPolicy,
+  () => Console.log("orElse").pipe(Effect.as("this is default value"))
+);
+Effect.runPromise(delayRepeated).then(console.log);
