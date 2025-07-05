771887ad98eb323080607dfda59a2970a8300b3b
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-07-05 15:11:31 +0900

    impl validate

--- lib/sample/try-effect-error-accumulation.ts
+++ lib/sample/try-effect-error-accumulation.ts
@@ -4,6 +4,7 @@ const task1 = Console.log("task1").pipe(Effect.as(1));
 const task2 = Effect.fail("omg").pipe(Effect.as(2));
 const task3 = Console.log("task2").pipe(Effect.as(3));
 const task4 = Effect.fail("omg2").pipe(Effect.as(4));
+const task5 = Effect.fail("omg3").pipe(Effect.as(5));
 
 const program = task1.pipe(
   Effect.zip(task2),
@@ -20,3 +21,12 @@ const forEachProgram = Effect.forEach([1, 2, 3, 4, 5], (n) => {
   }
 });
 Effect.runPromise(forEachProgram).then(console.log, console.error);
+
+// use validate
+const validateProgram = task1.pipe(
+  Effect.validate(task2),
+  Effect.validate(task3),
+  Effect.validate(task4),
+  Effect.validate(task5)
+);
+Effect.runPromiseExit(validateProgram).then(console.log);
