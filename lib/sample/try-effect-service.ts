952ad22b78d15a86095b02669ffa39304ee1a191
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-07-13 16:21:59 +0900

    impl MyLoggerService

--- lib/sample/try-effect-service.ts
+++ lib/sample/try-effect-service.ts
@@ -1,15 +1,23 @@
 import { Context, Effect } from "effect";
 import { MyRandomService } from "@/app/services/my_random_service";
+import { MyLoggerService } from "@/app/services/my_logger_service";
 
 const program = Effect.gen(function* () {
   const random = yield* MyRandomService;
+  const logger = yield* MyLoggerService;
+
   const randomNumber = yield* random.next;
-  console.log(`random number: ${randomNumber}`);
+  yield* logger.log(String(randomNumber));
 });
 
-const runnable = Effect.provideService(program, MyRandomService, {
-  next: Effect.sync(() => Math.random()),
-});
+const runnable = program.pipe(
+  Effect.provideService(MyRandomService, {
+    next: Effect.sync(() => Math.random()),
+  }),
+  Effect.provideService(MyLoggerService, {
+    log: (message) => Effect.sync(() => console.log(message)),
+  })
+);
 
 Effect.runPromise(runnable);
 
