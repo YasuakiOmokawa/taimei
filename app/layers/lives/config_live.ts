528906d312b8816a31f1a15401d67f8a6f1c1620
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-07-19 18:09:22 +0900

    remove of

--- app/layers/lives/config_live.ts
+++ app/layers/lives/config_live.ts
@@ -1,12 +1,9 @@
 import { ConfigService } from "@/app/services/config_service";
 import { Effect, Layer } from "effect";
 
-export const ConfigLive = Layer.succeed(
-  ConfigService,
-  ConfigService.of({
-    getConfig: Effect.succeed({
-      logLevel: "INFO",
-      connection: "mysql:username:password@hostname:port/database_name",
-    }),
-  })
-);
+export const ConfigLive = Layer.succeed(ConfigService, {
+  getConfig: Effect.succeed({
+    logLevel: "INFO",
+    connection: "mysql:username:password@hostname:port/database_name",
+  }),
+});
