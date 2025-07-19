96584d8c416d89296ed43032222ff5215e673844
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-07-19 18:08:06 +0900

    add config service interface

--- app/services/config_service.ts
+++ app/services/config_service.ts
@@ -1,6 +1,11 @@
-import { Context } from "effect";
+import { Context, Effect } from "effect";
 
 export class ConfigService extends Context.Tag("app/services/ConfigService")<
   ConfigService,
-  {}
+  {
+    readonly getConfig: Effect.Effect<{
+      readonly logLevel: string;
+      readonly connection: string;
+    }>;
+  }
 >() {}
