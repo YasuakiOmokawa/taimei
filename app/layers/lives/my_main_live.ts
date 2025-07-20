5fb5fe4c3452589db869059ab5a6069c29cbff72
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-07-20 11:58:03 +0900

    impl my main live

---
+++ app/layers/lives/my_main_live.ts
@@ -0,0 +1,9 @@
+import { Layer } from "effect";
+import { ConfigLive } from "./config_live";
+import { MyDatabaseLive } from "./my_database_live";
+import { AppConfigLive } from "./app_config_live";
+
+export const MyMainLive = MyDatabaseLive.pipe(
+  Layer.provide(AppConfigLive),
+  Layer.provide(ConfigLive)
+);
