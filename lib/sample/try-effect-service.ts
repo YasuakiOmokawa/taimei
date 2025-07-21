c1d9ff544e1200613514d9cc82e9a82df3d681bb
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-07-21 13:14:55 +0900

    impl layer launch

--- lib/sample/try-effect-service.ts
+++ lib/sample/try-effect-service.ts
@@ -1,4 +1,4 @@
-import { Context, Effect, Option, Clock, Console } from "effect";
+import { Context, Effect, Option, Clock, Console, Layer } from "effect";
 import { MyRandomService } from "@/app/services/my_random_service";
 import { MyLoggerService } from "@/app/services/my_logger_service";
 import { MyDatabaseService } from "@/app/services/my_database_service";
@@ -66,6 +66,7 @@ const DatabaseTest = MyDatabaseService.of({
   query: (_sql: string) => Effect.succeed([]),
 });
 import * as assert from "node:assert";
+import { HttpServerService } from "@/app/services/http_server_service";
 const test = Effect.gen(function* () {
   const database = yield* MyDatabaseService;
   const result = yield* database.query("select * from users");
@@ -84,3 +85,11 @@ const databaseProgram = Effect.gen(function* () {
 
 const runnableDatabaseProgram = Effect.provide(databaseProgram, MyMainLive);
 Effect.runPromise(runnableDatabaseProgram).then(console.log);
+
+// convert to effect from layer
+const server = Layer.effect(
+  HttpServerService,
+  Console.log("this is http server simulate")
+);
+Effect.runFork(Layer.launch(server));
+// sigkill
