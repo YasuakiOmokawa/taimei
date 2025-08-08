0feb3f9a275a9130c9f476ffc5e33f2aff4b0c64
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-08-08 14:47:20 +0900

    on failure

--- lib/sample/try-effect-resource-management.ts
+++ lib/sample/try-effect-resource-management.ts
@@ -2,8 +2,16 @@ import { Console, Effect } from "effect";
 
 const handler = Effect.ensuring(Console.log("cleanup completed."));
 
+// finalize on success
 const success = Console.log("task completed").pipe(
   Effect.as("some result"),
   handler
 );
 Effect.runPromise(success).then(console.log);
+
+// finalize on failure
+const failure = Console.log("task failed").pipe(
+  Effect.andThen(Effect.fail("some error")),
+  handler
+);
+Effect.runPromise(failure).catch(console.error);
