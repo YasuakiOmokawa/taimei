34b9d346949e6d6f27e8fb3a343dee6a94f33249
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-08-10 14:49:48 +0900

    lint ts

--- db/drizzle/relations.ts
+++ db/drizzle/relations.ts
@@ -1,64 +1,74 @@
 import { relations } from "drizzle-orm/relations";
-import { customers, invoices, user, session, userProfile, invoicesTotags, tags, authenticator, account } from "./schema";
+import {
+  customers,
+  invoices,
+  user,
+  session,
+  userProfile,
+  invoicesTotags,
+  tags,
+  authenticator,
+  account,
+} from "./schema";
 
-export const invoicesRelations = relations(invoices, ({one, many}) => ({
-	customer: one(customers, {
-		fields: [invoices.customerId],
-		references: [customers.id]
-	}),
-	invoicesTotags: many(invoicesTotags),
+export const invoicesRelations = relations(invoices, ({ one, many }) => ({
+  customer: one(customers, {
+    fields: [invoices.customerId],
+    references: [customers.id],
+  }),
+  invoicesTotags: many(invoicesTotags),
 }));
 
-export const customersRelations = relations(customers, ({many}) => ({
-	invoices: many(invoices),
+export const customersRelations = relations(customers, ({ many }) => ({
+  invoices: many(invoices),
 }));
 
-export const sessionRelations = relations(session, ({one}) => ({
-	user: one(user, {
-		fields: [session.userId],
-		references: [user.id]
-	}),
+export const sessionRelations = relations(session, ({ one }) => ({
+  user: one(user, {
+    fields: [session.userId],
+    references: [user.id],
+  }),
 }));
 
-export const userRelations = relations(user, ({many}) => ({
-	sessions: many(session),
-	userProfiles: many(userProfile),
-	authenticators: many(authenticator),
-	accounts: many(account),
+export const userRelations = relations(user, ({ many }) => ({
+  sessions: many(session),
+  userProfiles: many(userProfile),
+  authenticators: many(authenticator),
+  accounts: many(account),
 }));
 
-export const userProfileRelations = relations(userProfile, ({one}) => ({
-	user: one(user, {
-		fields: [userProfile.userId],
-		references: [user.id]
-	}),
+export const userProfileRelations = relations(userProfile, ({ one }) => ({
+  user: one(user, {
+    fields: [userProfile.userId],
+    references: [user.id],
+  }),
 }));
 
-export const invoicesTotagsRelations = relations(invoicesTotags, ({one}) => ({
-	invoice: one(invoices, {
-		fields: [invoicesTotags.a],
-		references: [invoices.id]
-	}),
-	tag: one(tags, {
-		fields: [invoicesTotags.b],
-		references: [tags.id]
-	}),
+export const invoicesTotagsRelations = relations(invoicesTotags, ({ one }) => ({
+  invoice: one(invoices, {
+    fields: [invoicesTotags.a],
+    references: [invoices.id],
+  }),
+  tag: one(tags, {
+    fields: [invoicesTotags.b],
+    references: [tags.id],
+  }),
 }));
 
-export const tagsRelations = relations(tags, ({many}) => ({
-	invoicesTotags: many(invoicesTotags),
+export const tagsRelations = relations(tags, ({ many }) => ({
+  invoicesTotags: many(invoicesTotags),
 }));
 
-export const authenticatorRelations = relations(authenticator, ({one}) => ({
-	user: one(user, {
-		fields: [authenticator.userId],
-		references: [user.id]
-	}),
+export const authenticatorRelations = relations(authenticator, ({ one }) => ({
+  user: one(user, {
+    fields: [authenticator.userId],
+    references: [user.id],
+  }),
 }));
 
-export const accountRelations = relations(account, ({one}) => ({
-	user: one(user, {
-		fields: [account.userId],
-		references: [user.id]
-	}),
+export const accountRelations = relations(account, ({ one }) => ({
+  user: one(user, {
+    fields: [account.userId],
+    references: [user.id],
+  }),
 }));
