af53e937c7891f680bfb7b5f2b0fb2eabb0a3178
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-09-22 14:33:34 +0900

    change dispays by cause

--- app/use-conform/(create)/create/page.tsx
+++ app/use-conform/(create)/create/page.tsx
@@ -1,25 +1,34 @@
 import Form from "@/app/ui/use-conform/create/form";
 import { runService } from "@/app/services";
 import { Tag2Service } from "@/app/services/tag2_service";
-import { Either } from "effect";
+import { Cause, Exit } from "effect";
 import { dateFormatter } from "@/dateFormatter";
 
 export default async function Page() {
-  const tag2sEither = await runService(() => Tag2Service.findAll());
+  const tag2sExit = await runService(() => Tag2Service.findAll());
 
-  if (Either.isLeft(tag2sEither)) {
-    return <div>{tag2sEither.left.toString()}</div>;
+  if (Exit.isFailure(tag2sExit)) {
+    return <div>{tag2sExit.toString()}</div>;
   }
 
-  const tag2One = tag2sEither.right.at(0);
+  const tag2One = tag2sExit.value.at(0);
   if (!tag2One) {
     return <div>no exists tag2</div>;
   }
 
-  const tag2Either = await runService(() => Tag2Service.findById(tag2One.id));
+  const tag2Exit = await runService(() => Tag2Service.findById(tag2One.id));
 
-  if (Either.isLeft(tag2Either)) {
-    return <div>{tag2Either.left.toString()}</div>;
+  if (Exit.isFailure(tag2Exit)) {
+    if (Cause.isFailType(tag2Exit.cause)) {
+      const err = tag2Exit.cause.error;
+      switch (err._tag) {
+        case "Tag2RepositoryError":
+          return <div>{`repo::: ${err.message}`}</div>;
+        default:
+          return <div>another error</div>;
+      }
+    }
+    return;
   }
 
   return (
@@ -34,7 +43,7 @@ export default async function Page() {
           </tr>
         </thead>
         <tbody>
-          {tag2sEither.right.map((tag2) => (
+          {tag2sExit.value.map((tag2) => (
             <tr key={tag2.id}>
               <td className="border">{tag2.id}</td>
               <td className="border">{tag2.name}</td>
@@ -45,12 +54,14 @@ export default async function Page() {
         </tbody>
       </table>
       <div className="mt-8 ml-8">
-        <ul className="list-disc">
-          <li>{tag2One.id}</li>
-          <li>{tag2One.name}</li>
-          <li>{dateFormatter.format(tag2One.createdAt)}</li>
-          <li>{dateFormatter.format(tag2One.updatedAt)}</li>
-        </ul>
+        {tag2Exit.value.map((tag2) => (
+          <ul className="list-disc" key={tag2.id}>
+            <li>{tag2.id}</li>
+            <li>{tag2.name}</li>
+            <li>{dateFormatter.format(tag2.createdAt)}</li>
+            <li>{dateFormatter.format(tag2.updatedAt)}</li>
+          </ul>
+        ))}
       </div>
       <Form />
     </main>
