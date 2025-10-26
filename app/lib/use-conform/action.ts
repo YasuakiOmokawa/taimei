6bdce45764916cbd5e0a4dc0b7b1b1cefc07194d
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-10-26 20:35:37 +0900

    enable form error handling

--- app/lib/use-conform/action.ts
+++ app/lib/use-conform/action.ts
@@ -5,6 +5,7 @@ import { schema } from "./schema";
 import { redirect } from "next/navigation";
 import { runService } from "@/app/services";
 import { ConformAccountResistrationService } from "@/app/services/conform-account-registration-service";
+import { Either } from "effect";
 
 export async function createData(_prevState: unknown, formData: FormData) {
   const submission = parseWithZod(formData, {
@@ -15,9 +16,21 @@ export async function createData(_prevState: unknown, formData: FormData) {
     return submission.reply();
   }
 
-  const _accountOrError = await runService(() =>
+  const accountOrError = await runService(() =>
     ConformAccountResistrationService.execute(submission.value)
   );
+
+  if (Either.isLeft(accountOrError)) {
+    const err = accountOrError.left;
+    switch (err._tag) {
+      case "AccountAlreadyExists":
+        return submission.reply({
+          fieldErrors: {
+            email: [err.message],
+          },
+        });
+    }
+  }
 
   redirect("/thanks");
 }
