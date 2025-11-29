0386e395da60d2863514632c86bdbeec11ab18b5
YasuakiOmokawa <omokawa@senk-inc.co.jp>
2025-11-29 20:04:04 +0900

    chore: remove Prisma files
    Prisma 関連ファイルを削除（Drizzle に移行完了）
    - prisma.ts, prisma/schema.prisma, prisma/seed.ts
    - prisma/migrations/ ディレクトリ
    - app/models/invoices.ts
    - app/lib/seedCreator/ ディレクトリ

--- prisma/migrations/20240902022555_add_tags2_model/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "tags2" (
-    "id" UUID NOT NULL DEFAULT gen_random_uuid(),
-    "name" VARCHAR(255) NOT NULL,
-
-    CONSTRAINT "tags2_pkey" PRIMARY KEY ("id")
-);
--- prisma/migrations/20241102030559_remove_updated_at_on_tag2/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `updated_at` on the `tags2` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "tags2" DROP COLUMN "updated_at";
